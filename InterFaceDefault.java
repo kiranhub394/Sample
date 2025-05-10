db717e35d668e45c3306e0f573423f271e5a55dd
Bankupalli Lakshmi Kirankumar <kirankumarbankupalli@gmail.com>
2025-05-10 16:15:26 +0530

    Add files via upload

---
+++ InterFaceDefault.java
@@ -0,0 +1,13 @@
+package com.practice2;
+//accessing the interfaces using multiple inheritance in java 8
+public class InterFaceDefault{
+
+	public void show() {
+		
+		System.out.println("Calss Method");
+	
+	}
+
+	
+
+}
